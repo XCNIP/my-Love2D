519976814cac7a62ef4028101d8a3e79398b2f3e
xcnip <2034831630@qq.com>
2026-09-20 17:54:09 +0800

    Enhance pacman movement and rotation logic for improved gameplay dynamics

--- main.lua
+++ main.lua
@@ -19,6 +19,9 @@ function love.load()
     pacman.size = 100
     pacman.eat = false
     pacman.speed = 2
+    pacman.rotation = 0
+    pacman.angle1 = 3.14 * 30 / 180 + pacman.rotation
+    pacman.angle2 = 3.14 * 330 / 180 + pacman.rotation
     love.graphics.setBackgroundColor(.3, .5, .5)
     _G.food = {
         x     = 600,
@@ -26,9 +29,17 @@ function love.load()
         size  = 100,
         eaten = false,
     }
+    _G.tick = 0
+    _G.flag = false
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("down") then
+        pacman.rotation = (pacman.rotation + 3.14/180) % 6.18 
+    end
+    if love.keyboard.isDown("up") then
+        pacman.rotation = (pacman.rotation - 3.14/180) % 6.18 
+    end
     if love.keyboard.isDown("a") then
         pacman.x = pacman.x - 1
     end
@@ -44,6 +55,21 @@ function love.update(dt)
     if pacman.x >= food.x + 50 then
         food.eaten = true
     end
+    
+    _G.tick = tick + 1
+
+    if tick >= 10 then
+        _G.tick = 0
+        if _G.flag == false then
+            _G.flag = true
+            pacman.angle1 = 0
+            pacman.angle2 = 10
+        else
+            _G.flag = false
+            pacman.angle1 = 3.14 * 30 / 180 + pacman.rotation
+            pacman.angle2 = 3.14 * 330 / 180 + pacman.rotation
+        end
+    end
 end
 
 function love.draw()
@@ -54,5 +80,7 @@ function love.draw()
     -- love.graphics.rectangle( mode, x, y, width, height, rx, ry, segments )
     love.graphics.setColor(1, .5, .3)
     -- love.graphics.circle("line",100, 400, 40)
-    love.graphics.arc("fill", pacman.x, pacman.y, pacman.size, 3.14 * 30 / 180, 3.14 * 330 / 180)
+    love.graphics.arc("fill", pacman.x, pacman.y, pacman.size, pacman.angle1,pacman.angle2 )
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(pacman.rotation, 0, 0)
 end
