229b9a4a0ddf607402e715e4915b112dcf813c79
xcnip <2034831630@qq.com>
2026-09-20 16:23:27 +0800

    Fix version number in configuration and refactor food properties in main game script

--- conf.lua
+++ conf.lua
@@ -9,7 +9,7 @@
 --]]
 function love.conf(t)
     t.identity = "data/saves"
-    t.version  = "0.1"
+    t.version  = "0.0.1"
     t.console = false
     t.externalstorage = true
     t.gammacorrect = true
--- main.lua
+++ main.lua
@@ -11,34 +11,36 @@
 _G.love = require("love") -- import love modules
 
 function love.load()
-     _G.num = 0    
-     _G.color_R = 0
-     _G.pacman = {}
-     pacman.x = 200
-     pacman.y = 200
-     pacman.size = 100
-     pacman.eat = false
-     love.graphics.setBackgroundColor(.3, .5, .5)
-     _G.food_x = 600
-     _G.food_y = 150
-     _G.food_size = 100
+    _G.num = 0
+    _G.color_R = 0
+    _G.pacman = {}
+    pacman.x = 200
+    pacman.y = 200
+    pacman.size = 100
+    pacman.eat = false
+    love.graphics.setBackgroundColor(.3, .5, .5)
+    _G.food = {
+        x     = 600,
+        y     = 150,
+        size  = 100,
+        eaten = false,
+    }
 end
 
 function love.update(dt)
     pacman.x = pacman.x + 2
-    if pacman.x >= food_x + 50 then 
-        pacman.eat = true
+    if pacman.x >= food.x + 50 then
+        food.eaten = true
     end
 end
 
 function love.draw()
-    if not pacman.eat then
+    if not food.eaten then
         love.graphics.setColor(0, 0, 0)
-        love.graphics.rectangle("fill", food_x, food_y, food_size, food_size)
+        love.graphics.rectangle("fill", food.x, food.y, food.size, food.size)
     end
     -- love.graphics.rectangle( mode, x, y, width, height, rx, ry, segments )
     love.graphics.setColor(1, .5, .3)
     -- love.graphics.circle("line",100, 400, 40)
     love.graphics.arc("fill", pacman.x, pacman.y, pacman.size, 3.14 * 30 / 180, 3.14 * 330 / 180)
 end
-
